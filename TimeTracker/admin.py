7a075407065797155badf875992a2f2b4cf08623
Mohamad <Moenaser4@gmail.com>
2020-12-16 18:43:39 +0200

    Creating Models

--- TimeTracker/admin.py
+++ TimeTracker/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from .models import Employee, Company, Post
 
-# Register your models here.
+admin.site.register(Post)
+admin.site.register(Employee)
+admin.site.register(Company)
--- TimeTracker/models.py
+++ TimeTracker/models.py
@@ -1,3 +1,22 @@
 from django.db import models
+from django.utils import timezone
 
-# Create your models here.
+
+class Company(models.Model):
+    name = models.CharField(max_length=50)
+
+
+class Employee(models.Model):
+    name = models.CharField(max_length=65)
+    position = models.CharField(max_length=50)
+    company = models.ForeignKey('Company', on_delete=models.CASCADE)
+
+
+class Post(models.Model):
+    title = models.CharField(max_length=200)
+    text = models.TextField()
+    created_date = models.DateTimeField(default=timezone.now)
+    start_time = models.DateTimeField(auto_now_add=True, blank=True)
+    end_time = models.DateTimeField(auto_now_add=True, blank=True)
+    employee = models.ForeignKey('Employee', on_delete=models.CASCADE)
+
