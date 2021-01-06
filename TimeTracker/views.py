98340faf39b4ac052afb0f7e47422b6b48d9c600
Mohamad <Moenaser4@gmail.com>
2021-01-06 03:10:32 +0200

    Authentication System

--- TimeTracker/views.py
+++ TimeTracker/views.py
@@ -1,7 +1,23 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import MyCustomSignupForm
+from django.contrib.auth import authenticate, login
 
 
-# Create your views here.
 def home(request):
     return render(request, "layout/home.html", {})
 
+
+def signup(request):
+    if request.methods == 'POST':
+        form = MyCustomSignupForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data['username']
+            password = form.cleaned_data['password1']
+            user = authenticate(username=username, password=password)
+            login(request, user)
+            return redirect('/accounts/profile')
+    else:
+        form = MyCustomSignupForm()
+
+    return render(request, 'account/signup.html', {'form': form})
--- time_tracker/urls.py
+++ time_tracker/urls.py
@@ -13,11 +13,17 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls import url
 from django.contrib import admin
-from django.urls import path
+from django.urls import path, include
+from django.views.generic import TemplateView
+
 from TimeTracker import views
 
 urlpatterns = [
+    path('', TemplateView.as_view(template_name="layout/home.html")),
     path('admin/', admin.site.urls),
     path('', views.home, name='home'),
+    url(r'^accounts/', include('allauth.urls')),
+    path('', include('TimeTracker.urls')),
 ]
